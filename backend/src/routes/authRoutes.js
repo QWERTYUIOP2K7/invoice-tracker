b63c27387e504dd76db51af4450cd4446e685f8c
QWERTYUIOP2K7 <aabhassaxena2007@gmail.com>
2026-07-17 14:32:58 +0530

    auth routes fix

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
-const { registerClient } = require('../controllers/authController');
+const {
+  login,
+  getMe,
+  registerClient,
+} = require('../controllers/authController');
 const router = express.Router();
 router.post('/register-client', registerClient);
-router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 
